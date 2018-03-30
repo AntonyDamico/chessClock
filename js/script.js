5466f1b8bd088cd33c9480fd95723eeb16aaf03f
Antony Damico <antonydamico29@gmail.com>
2018-03-29 20:52:30 -0400

    giving functionality to the buttons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 //Current player
 var current = false;
-var startTime = 190;
+var startTime = 5;
 var buttons = document.querySelectorAll("button");
 
 
 //Counter object, it contains the current time of the counter and
 // the document object of the counter
-var Counter = function(display, time){
+var Counter = function(display, time) {
 	this.time = time;
 	this.display = display;
 	display.textContent = convertSeconds(this.time);
 
 	// display.textContent = convertSeconds(this.time);
-	this.passTime = function(){
-		if(this.time > 0){
+	this.passTime = function() {
+		if (this.time > 0) {
 			this.time--;
 			display.textContent = convertSeconds(this.time);
+		} else {
+			buttons[+current].disabled = true;
+			alert("Player " + +current + " ran out of time")
+			//Exits out of the setInterval
+			clearInterval(1);
 		}
 	}
 }
@@ -29,9 +34,11 @@ var counters = [
 
 //Event listener to change the current player's timer 
 for (var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", function(){
+	buttons[i].addEventListener("click", function() {
+		buttons[+current].disabled = true;
 		current = !current;
 		console.log("tic")
+		buttons[+current].disabled = false;
 	});
 }
 
@@ -40,16 +47,18 @@ function convertSeconds(s) {
 	var min = Math.floor(s / 60);
 	var sec = s % 60;
 	var extra = "";
-	if(sec < 10){
+	//If the seconds are less than 10, the extra will give them a 0 on front
+	if (sec < 10) {
 		extra = "0";
 	}
 	return min + ":" + extra + sec;
 }
 
 
-function init(){
+function init() {
 	//The unary + operator converts a boolean into an integer
-	counters[+current].passTime(); 
+	//+1 is true and +0 is false
+	counters[+current].passTime();
 };
 
 
